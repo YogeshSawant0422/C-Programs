c8bb364187a3be21353524779ba4f0b7364713ea
Yogesh Sawant <yogeshsawant0422@gmail.com>
2021-01-26 13:41:07 +0530

    modified

--- Even or Odd/Even Odd using ternary.c
+++ Even or Odd/Even Odd using ternary.c
@@ -5,15 +5,15 @@
 int main()
 {
     int no = 0;
-    //yogi:
+    yogi:
     printf("\n Enter an integer number ==>>");
     scanf("\n%d",&no);
 
-    /*if(no == 0)
+    if(no == 0)
     {
         printf("\n Given Number is neutral");
         goto yogi;
-    }*/
+    }
 
     (no%2==0)? printf("\n Given number %d is even",no):printf("\n Given number %d is odd",no);
 
